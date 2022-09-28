4997bb44d9145381b5531827d4f5e0291c120a21
Matatias <matatias97@gmail.com>
2022-09-29 01:03:22 +0700

    chore:Use Valuenotifier

--- lib/src/input_qty.dart
+++ lib/src/input_qty.dart
@@ -40,13 +40,20 @@ class InputQty extends StatefulWidget {
 }
 
 class _InputQtyState extends State<InputQty> {
+  /// text controller of textfield
   TextEditingController _valCtrl = TextEditingController();
-  late num value;
+
+  /// current value of quantity
+  /// late num value;
+  late ValueNotifier<num> currentval;
 
   @override
   void initState() {
-    value = widget.initVal;
+    // value = widget.initVal;
+    currentval = ValueNotifier(widget.initVal);
+
     _valCtrl = TextEditingController(text: "${widget.initVal}");
+
     widget.onQtyChanged(num.tryParse(_valCtrl.text));
     super.initState();
   }
@@ -55,18 +62,24 @@ class _InputQtyState extends State<InputQty> {
   /// based on stpes
   /// default steps = 1
   void plus() {
-    value = num.parse(_valCtrl.text);
+    num value = num.parse(_valCtrl.text);
 
     if (value < widget.maxVal) {
-      setState(() {
-        value += widget.steps;
-      });
+      value += widget.steps;
+      currentval = ValueNotifier(value);
+      // setState(() {
+      //   value += widget.steps;
+      // });
     } else {
-      setState(() {
-        value = widget.maxVal;
-      });
+      value = widget.maxVal;
+      currentval = ValueNotifier(value);
+      // setState(() {
+      //   value = widget.maxVal;
+      // });
     }
     _valCtrl.text = "$value";
+
+    /// move cursor to the right side
     _valCtrl.selection =
         TextSelection.fromPosition(TextPosition(offset: _valCtrl.text.length));
     widget.onQtyChanged(num.tryParse(value.toString()));
@@ -76,17 +89,24 @@ class _InputQtyState extends State<InputQty> {
   /// based on stpes
   /// default steps = 1
   void minus() {
-    value = num.parse(_valCtrl.text);
+    num value = num.parse(_valCtrl.text);
     if (value > 0) {
-      setState(() {
-        value -= widget.steps;
-      });
+      value -= widget.steps;
+      currentval = ValueNotifier(value);
+
+      // setState(() {
+      //   value -= widget.steps;
+      // });
     } else {
-      setState(() {
-        value = 0;
-      });
+      value = widget.minVal;
+      currentval = ValueNotifier(value);
+      // setState(() {
+      //   value =widget.minVal;
+      // });
     }
     _valCtrl.text = "$value";
+
+    /// move cursor to the right side
     _valCtrl.selection =
         TextSelection.fromPosition(TextPosition(offset: _valCtrl.text.length));
     widget.onQtyChanged(num.tryParse(value.toString()));
@@ -125,40 +145,43 @@ class _InputQtyState extends State<InputQty> {
               alignment: Alignment.center,
               margin: const EdgeInsets.symmetric(horizontal: 20.0),
               child: IntrinsicWidth(
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    isDense: true,
-                    isCollapsed: true,
-                  ),
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                  ),
-                  controller: _valCtrl,
-                  onChanged: (String strVal) {
-                    num? temp = num.tryParse(_valCtrl.text);
-                    if (temp == null) return;
+                child: Builder(builder: (context) {
+                  print('textfield rebuild');
+                  return TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      isDense: true,
+                      isCollapsed: true,
+                    ),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                    controller: _valCtrl,
+                    onChanged: (String strVal) {
+                      num? temp = num.tryParse(_valCtrl.text);
+                      if (temp == null) return;
 
-                    if (temp > widget.maxVal) {
-                      temp = widget.maxVal;
-                      _valCtrl.text = "${widget.maxVal}";
-                    } else if (temp <= widget.minVal) {
-                      temp = widget.minVal;
-                      _valCtrl.text = temp.toString();
-                    }
-                    widget.onQtyChanged(num.tryParse(_valCtrl.text));
+                      if (temp > widget.maxVal) {
+                        temp = widget.maxVal;
+                        _valCtrl.text = "${widget.maxVal}";
+                      } else if (temp <= widget.minVal) {
+                        temp = widget.minVal;
+                        _valCtrl.text = temp.toString();
+                      }
+                      widget.onQtyChanged(num.tryParse(_valCtrl.text));
 
-                    _valCtrl.selection = TextSelection.fromPosition(
-                        TextPosition(offset: _valCtrl.text.length));
-                  },
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [
-                    // LengthLimitingTextInputFormatter(10),
-                    FilteringTextInputFormatter.allow(RegExp(r"^\d*\.?\d*")),
-                  ],
-                ),
+                      _valCtrl.selection = TextSelection.fromPosition(
+                          TextPosition(offset: _valCtrl.text.length));
+                    },
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      // LengthLimitingTextInputFormatter(10),
+                      FilteringTextInputFormatter.allow(RegExp(r"^\d*\.?\d*")),
+                    ],
+                  );
+                }),
               ),
             ),
             const SizedBox(
