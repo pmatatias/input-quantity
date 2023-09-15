8cf48aed904d5bee73a7287af7c43e54dffb2935
Matatias <matatias97@gmail.com>
2023-09-15 17:32:40 +0700

    update

---
+++ example/lib/charts/model.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+
+class ProductModel {
+  final int id;
+  final String title;
+  final String description;
+  final int price;
+  final double discountPercentage;
+  final double rating;
+  final int stock;
+  final String brand;
+  final String category;
+  final String thumbnail;
+  final List<String> images;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.discountPercentage,
+    required this.rating,
+    required this.stock,
+    required this.brand,
+    required this.category,
+    required this.thumbnail,
+    required this.images,
+  });
+
+  ProductModel copyWith({
+    int? id,
+    String? title,
+    String? description,
+    int? price,
+    double? discountPercentage,
+    double? rating,
+    int? stock,
+    String? brand,
+    String? category,
+    String? thumbnail,
+    List<String>? images,
+  }) =>
+      ProductModel(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        price: price ?? this.price,
+        discountPercentage: discountPercentage ?? this.discountPercentage,
+        rating: rating ?? this.rating,
+        stock: stock ?? this.stock,
+        brand: brand ?? this.brand,
+        category: category ?? this.category,
+        thumbnail: thumbnail ?? this.thumbnail,
+        images: images ?? this.images,
+      );
+
+  factory ProductModel.fromRawJson(String str) =>
+      ProductModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+        id: json["id"],
+        title: json["title"],
+        description: json["description"],
+        price: json["price"],
+        discountPercentage: json["discountPercentage"]?.toDouble(),
+        rating: json["rating"]?.toDouble(),
+        stock: json["stock"],
+        brand: json["brand"],
+        category: json["category"],
+        thumbnail: json["thumbnail"],
+        images: List<String>.from(json["images"].map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "description": description,
+        "price": price,
+        "discountPercentage": discountPercentage,
+        "rating": rating,
+        "stock": stock,
+        "brand": brand,
+        "category": category,
+        "thumbnail": thumbnail,
+        "images": List<dynamic>.from(images.map((x) => x)),
+      };
+}
---
+++ example/lib/charts/view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Example1 extends StatefulWidget {
+  const Example1({super.key});
+
+  @override
+  State<Example1> createState() => _Example1State();
+}
+
+class _Example1State extends State<Example1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Example 1")),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
